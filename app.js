06ead5e2af29cfee8732a94c1902b884014e2be8
trbznk <alexander.terbeznik@icloud.com>
2024-05-04 19:32:12 +0200

    first prototype to play around with nash equilibrium

--- app.js
+++ app.js
@@ -1,35 +1,74 @@
-const canvas = document.getElementById("chart");
-const ctx = canvas.getContext("2d");
-const N_SIMS = 1000;
-const X_GRID_SIZE = canvas.width/N_SIMS;
-const Y_GRID_SIZE = 1.5*canvas.height/N_SIMS;
+async function main() {
+    const N_SIMS = 250;
+    let RESULTS = [[],[]];
+    let TIMEOUT = 10;
 
-console.log(canvas.width, canvas.height);
-console.log(X_GRID_SIZE, Y_GRID_SIZE)
+    let pm = parseFloat(document.getElementById("pm_input").value)/100;
+    let pf = 1-parseFloat(document.getElementById("pf_input").value)/100;
 
-// probability to go to the football stadium
-let pm = 0.75;
-let pf = 0.25;
-
-let meets = 0;
-let results = [];
-for (let i = 0; i < N_SIMS; i++) {
-    let m = Math.random();
-    let f = Math.random();
-
-    if (m <= pm && f <= pf || m > pm && f > pf) {
-        meets++;
+    const canvas = document.getElementById("chart");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 800;
+    canvas.height = 600;
+    
+    for (let i = 0; i < N_SIMS+1; i++) {
+        let m = Math.random();
+        let f = Math.random();
+        if (m <= pm && f <= pf) {
+            RESULTS[0].push(3);
+            RESULTS[1].push(2);
+        } else if (m > pm && f > pf) {
+            RESULTS[0].push(2);
+            RESULTS[1].push(3);
+        } else {
+            if (m <= pm) {
+                RESULTS[0].push(1);
+            } else {
+                RESULTS[0].push(0);
+            }
+            if (f <= pf) {
+                RESULTS[1].push(0);
+            } else {
+                RESULTS[1].push(1);
+            }
+        }
     }
-    results.push(meets);
 
-    xLast = i * X_GRID_SIZE;
-    yLast = canvas.height- (results[i-1] * Y_GRID_SIZE);
-    xNext = (i+1) * X_GRID_SIZE;
-    yNext = canvas.height - (results[i] * Y_GRID_SIZE);
-    ctx.beginPath();
-    ctx.moveTo(xLast, yLast);
-    ctx.lineTo(xNext, yNext);
-    ctx.stroke();
+    let RESULTS_CUMSUM = [[],[]];
+    for (let i = 0; i < N_SIMS; i++) {
+        if (i == 0) {
+            RESULTS_CUMSUM[0].push(RESULTS[0][i]);
+            RESULTS_CUMSUM[1].push(RESULTS[1][i]);
+        } else {
+            RESULTS_CUMSUM[0].push(RESULTS_CUMSUM[0][i-1] + RESULTS[0][i]);
+            RESULTS_CUMSUM[1].push(RESULTS_CUMSUM[1][i-1] + RESULTS[1][i]);
+        }
+    }
+
+    let simulations_span = document.getElementById("simulations");
+    for (let frame = 0; frame < N_SIMS; frame++) {
+        simulations_span.innerHTML = frame+1;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.lineWidth = 5;
+        ctx.lineCap = "round";
+        let max_y_value = Math.max(RESULTS_CUMSUM[0][frame-1], RESULTS_CUMSUM[1][frame-1]);
+        let x_grid_size = (canvas.width)/frame;
+        let y_grid_size = ((4/5)*canvas.height)/max_y_value;
+        for (let g = 0; g < 2; g++) {
+            ctx.beginPath();
+            ctx.strokeStyle = ["green", "blue"][g];
+            let payoff_span = [document.getElementById("payoff_m"), document.getElementById("payoff_f")][g];
+            for (let i = 0; i < frame; i++) {
+                let x = i*x_grid_size;
+                let y = canvas.height - RESULTS_CUMSUM[g][i]*y_grid_size;
+                payoff_span.innerHTML = (RESULTS_CUMSUM[g][i] / (i+1)).toFixed(2);
+                ctx.lineTo(x, y);
+            }
+            ctx.stroke();
+        }
+        await new Promise(r => setTimeout(r, TIMEOUT));
+    }
 }
-
-console.log(results);
+            
+            
+            
